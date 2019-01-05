2a5cd5b4ea46985874e68e1f89c0d19ed15a178b
Himanshu Mundhra <himanshumundhra98@gmail.com>
2019-01-06 01:45:11 +0530

    Add files via upload

---
+++ CodeForces Codes/1099-C： Postcard.cpp
@@ -0,0 +1,144 @@
+//https://codeforces.com/contest/1099/problem/C
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef unsigned long long ul ;
+typedef long long ll ;
+typedef string str ;
+
+typedef pair < int , int >  pii ;
+typedef pair < ll , ll>  pll ;
+typedef pair < ll , pll > edge ; 
+typedef vector < int > vi ;
+typedef vector < ll > vll ;
+typedef vector < pll > vp ;
+
+#define MOD 1000000007  
+#define vec vector    
+#define nl '\n' 
+#define is == 
+#define all(v) (v).begin() , (v).end() 
+#define sumof(v) accumulate ( all(v) , 0LL )
+#define maxof(v) max_element( all(v) )
+#define minof(v) min_element( all(v) )
+#define rev(v) reverse(all(v)) 
+#define sz(v)  (long long)(v).size() 
+#define lp(i , n ) for ( ll i = 0 ; i < (n) ; i++ )
+#define lp1(i , n ) for ( ll i = 1 ; i <= (n) ; i++ )
+#define lpp(i , n ) for ( ll i = n-1 ; i >= 0 ; i-- )      
+#define umap unordered_map 
+
+#define F first 
+#define S second
+#define pb push_back
+#define eb emplace_back
+
+#define tc(t) ul testcases ; cin>>testcases ; for ( int t = 1 ; t <= testcases ; t++ )
+#define prntc cout<<"Case "<<testcases<<" : " ; 
+#define inarr(size) vll v ( size , 0 ) ; lp ( i , size ) cin>>v[i]  
+#define invec( vec_name ) lp ( i , sz(vec_name) ) cin>>vec_name[i] 
+#define debug( ds_name ) for ( auto val : ds_name ) cerr<<val<<" " ; cerr<<nl 
+#define debug1(x) cerr<<#x<<" :: "<<x<<"\n";
+#define debug2(x,y) cerr<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define debug3(x,y,z) cerr<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\t"<<#z<<" :: "<<z<<"\n";
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+
+ll modExp(ll x,ll y,ll mod){ ll res = 1; while(y){if(y&1) res= (res*x)%mod; y/=2, x=(x*x)%mod; }return res%mod;}
+vll calc_fact (ll n , ll mod ){    vll fact(n+1,1) ; lp1 (i,n) fact[i] = (fact[i-1]*i) % mod ; return fact ; }
+vll FF( str A ) { vll LPB(sz(A),0); ll len = 0 ; lp1 ( i , sz(A)-1) {if ( A[i] is A[len] ) LPB[i] = ++len ; else if ( len ) len = LPB[len-1] , i-- ; } return LPB ; }
+bool KMP( str A, str B, vll LPB ){ ll j=0 ; lp (i,sz(A)) {if ( A[i] is B[j]) {if ( ++j is B.size() ) return true ;} else if (j) {j = LPB[j-1] ; i-- ; } } return false ;}
+
+vll factorial = { 1 , 1 , 2 , 6 , 24 , 120 , 720 , 5040 , 40320 , 362880 , 3628800 , 39916800 ,  
+479001600 , 6227020800 , 87178291200, 1307674368000 , 20922789888000 , 355687428096000 , 6402373705728000    } ; //18!  
+
+string no = "Impossible\n" ; 
+
+int main () 
+{
+	fast 
+
+	string s ; cin>>s ;
+	ll k ; cin>>k ; 
+
+	ll q = 0 , st = 0 ; 
+	lp ( i , sz(s) ) q += ( s[i] is '?' ) , st += ( s[i] is '*' ) ; 
+
+	ll mini = sz(s) - 2*q - 2*st ; 
+	ll maxi = INT_MAX ;
+	if ( st is 0 )
+	{
+		maxi = sz(s) - q ; 
+	}
+
+	if ( k < mini or maxi < k ) {
+		cout<<no ; return 0 ; 	
+	}
+
+	string ans ; 
+
+	if ( st == 0 )
+	{
+		ll set = k - mini ; 	
+
+		ll c = 0 ; 
+		lp ( i , sz(s) )
+		{
+			ans.pb(s[i]) ; 
+			if ( s[i] is '?' )
+			{
+				ans.pop_back() ;
+				if ( c < set )
+				{
+					c++ ; 
+				}		
+				else
+				{
+					ans.pop_back() ;
+				}	
+			}
+		}
+		cout<<ans<<nl ; 
+	}
+	else
+	{
+		ll done = 0 ; 
+		ll rep = k - mini ; 
+
+		lp ( i , sz(s) )
+		{
+			ans.pb(s[i]) ; 
+			if ( s[i] is '?' )
+			{
+				ans.pop_back() ; 
+				ans.pop_back() ;
+			}
+			else if ( s[i] is '*' )
+			{
+				ans.pop_back() ;
+				ans.pop_back() ;
+
+				if ( done is 0 )
+				{
+					for ( int j = 0 ; j < rep ; j++ )
+					{
+						ans.pb(s[i-1]) ;
+					}
+					done = 1 ; 
+				}				
+			}
+		}
+		cout<<ans<<nl ; 
+	}
+
+
+	return 0 ; 
+}
