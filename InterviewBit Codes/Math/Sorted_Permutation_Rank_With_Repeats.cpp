a69c3601597f0425f04fac38317104f7f805c120
Himanshu Mundhra <himanshumundhra98@gmail.com>
2019-01-06 00:40:53 +0530

    Update Sorted_Permutation_Rank_With_Repeats.cpp

--- InterviewBit Codes/Math/Sorted_Permutation_Rank_With_Repeats.cpp
+++ InterviewBit Codes/Math/Sorted_Permutation_Rank_With_Repeats.cpp
@@ -1,85 +1,52 @@
 // https://www.interviewbit.com/problems/sorted-permutation-rank-with-repeats/
 
-#define MOD 1000003 
-unsigned long long  modexp ( unsigned long long  base , unsigned long long  expo , unsigned long long  modulo )
-{
-    base = base % modulo ;
-    unsigned long long  ans = 1 ;
-    while ( expo )
-    {
-        if ( expo&1 )
-            ans = (ans * base) % modulo ;
+typedef long long ll ;
+typedef vector < ll > vll ;
+#define MOD 1000003  
+#define sz(v)  (long long)(v).size() 
 
-        expo >>= 1 ;
-        base = (base*base)% modulo  ;
+ll modExp(ll x,ll y,ll mod){ ll res = 1; while(y){if(y&1) res= (res*x)%mod; y/=2, x=(x*x)%mod; }return res%mod;}
+vll calc_fact (ll n , ll mod ){    vll fact(n+1,1) ; for ( ll i = 1 ; i <= (n) ; i++ ) fact[i] = (fact[i-1]*i) % mod ; return fact ; }
 
-    }
-    return ans ;
-}
-
-unsigned long long  fact ( unsigned long long  n )
-{
-    unsigned long long  ans = 1 ; 
-    while(n) 
-    {
-        ans = ( (ans%MOD) * (n%MOD ) ) % MOD ; 
-        n-- ; 
-    }
-    
-    return (ans%MOD) ; 
-}
-
-int index ( char c ) 
-{
-    if ( isupper(c) )
-    {
-        return ( c - 'A' ) ; 
-    }
-    else
-    {
-        return ( c - 'a' + 26 ) ; 
-    }
-}
+ll index ( char c ){ if ( isupper(c) ) return ( c - 'A' ) ; return ( c - 'a' + 26 ) ; }
 
 int Solution::findRank(string A) {
     
-    if ( A.size() == 0 ) return 0 ; if ( A.size() == 1 ) return 1 ;
+    if ( sz(A) <= 1 ) return sz(A) ; 
+
+    vll fact = calc_fact(sz(A),MOD) ; 
     
-    vector < int > count ( 52 , 0 ) ; 
-    for ( int i = 0 ; i < A.size() ; i++ )
-    {
+    vll count ( 52 , 0 ) ; 
+    for ( ll i = 0 ; i < sz(A) ; i++ ) {
         count[ index(A[i]) ] ++ ; 
     }
-    
-    
-    unsigned long long  perm = 1 ; 
-    for ( int i = 0 ; i < 52 ; i++ )
-            perm  *= fact(count[i]) ;
-         
+        
+    ll  perm = 1 ; 
+    for ( int i = 0 ; i < 52 ; i++ ) {
+            perm  = ( perm * fact[ count[i] ] ) % MOD ;
+    }   
             
-    unsigned long long num = 1 ; 
-    for ( int i = 0 ; i < A.size() ; i++ )
-    {   
-        //cout<<perm<<"##" ;
+    ll num = 1 ; 
+    for ( int i = 0 ; i < sz(A) ; i++ )
+    {          
         for ( int j = 0 ; j < index( A[i] ) ; j++ )
         {
             if ( count[j] != 0 )
             {   
                 //cout<<j<<"*" ; 
-                unsigned long long ans ;
-                ans =  ( count[j] % MOD * fact(A.size()-1 -i) % MOD ) % MOD ; 
-                ans *= modexp( perm , MOD-2 , MOD ) ; 
+                ll ans ;
+                ans =  ( count[j] % MOD * fact[sz(A) -1 -i] % MOD ) % MOD ; 
+                ans *= modExp( perm , MOD-2 , MOD ) ; 
                 ans %= MOD ; 
                 //cout<<ans<<"*" ; 
                 num = (num + (ans)%MOD)%MOD ; 
             }
         }
-        perm /= count[ index( A[i] ) ] ; 
+        ll value = modExp ( count[ index( A[i] ) ]  , MOD-2 , MOD ) ; 
+        perm = ( perm * value ) % MOD ; 
         count[ index( A[i] ) ] -- ; 
         //cout<<num<<endl ; 
-    }
-    
-    return num ; 
-    
+    }    
+    return num ;     
     
 }
