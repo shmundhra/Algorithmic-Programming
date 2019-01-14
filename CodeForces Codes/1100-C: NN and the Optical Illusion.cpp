55bb990ad65a025449df5fec60c51b003770c53a
Himanshu Mundhra <himanshumundhra98@gmail.com>
2019-01-14 21:42:42 +0530

    Add files via upload

---
+++ CodeForces Codes/1100-C: NN and the Optical Illusion.cpp
@@ -0,0 +1,67 @@
+//https://codeforces.com/contest/1100/problem/C
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+typedef unsigned long long ul ; typedef long long ll ; typedef string str ;
+typedef pair < int , int >  pii ; typedef pair < ll , ll>  pll ;
+typedef vector < int > vi ; typedef vector < ll > vll ;
+
+//#define cerr cout 
+#define MOD 1000000007 
+#define vec vector    
+#define nl '\n' 
+#define is == 
+#define all(v) (v).begin() , (v).end() 
+#define sumof(v) accumulate ( all(v) , 0LL )
+#define maxof(v) max_element( all(v) )
+#define minof(v) min_element( all(v) )
+#define rev(v) reverse(all(v)) 
+#define sz(v)  (long long)(v).size() 
+#define lp(i , n ) for ( ll i = 0 ; i < (n) ; i++ )
+#define lpp(i , l , r ) for ( ll i = (l) ; i < (r) ; i++ ) 	 
+#define umap unordered_map 
+
+#define F first 
+#define S second
+#define pb push_back
+#define eb emplace_back
+
+#define tc(t) ul testcases ; cin>>testcases ; for ( int t = 1 ; t <= testcases ; t++ )
+#define prntc cout<<"Case "<<testcases<<" : " ; 
+#define inarr(size) vll v ( size , 0 ) ; lp ( i , size ) cin>>v[i]  
+#define invec( vec_name ) lp ( i , sz(vec_name) ) cin>>vec_name[i] 
+#define debug( ds_name ) for ( auto &val : ds_name ) cerr<<val<<"_" ; cerr<<nl 
+#define debug1(x) cerr<<#x<<" :: "<<x<<"\n";
+#define debug2(x,y) cerr<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\n";
+#define debug3(x,y,z) cerr<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\t"<<#z<<" :: "<<z<<"\n";
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+#define PI 3.1415926
+
+int main () 
+{
+	fast	
+	double n , r ; 
+	cin>>n>>r ; 
+
+	double angle = ( 2.0 * PI ) / n ; 
+	double cosine = cos(angle) ; 
+	double val1 = 1-cosine ; 
+	double val2 = 2.0 / val1 ; 
+	double val3 = sqrt(val2) ; 
+	double val4 = val3-1 ;
+
+	double ans = r / val4 ; 
+
+	printf("%.10lf" , ans )   ; 
+
+	return 0 ; 
+}
