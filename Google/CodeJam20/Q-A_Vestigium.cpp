a8788450db6d6d03117e62b8a898acdebe0a0e1e
shmundhra <himanshumundhra98@gmail.com>
2020-04-11 15:38:06 +0530

    Add CodeJam20 QualificationRound Codes

---
+++ Google/CodeJam20/Q-A_Vestigium.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ul ;
+typedef long long ll ;
+typedef string str ;
+typedef pair <int, int >  pii ;
+typedef pair <ll, ll>  pll ;
+typedef vector <int> vi ;
+typedef vector <bool> vb ;
+typedef vector <ll> vll ;
+typedef vector <pii> vpi ;
+typedef vector <pll> vpl ;
+#define umap unordered_map
+
+//#define cerr cout
+#define MOD 1000000007
+#define vec vector
+#define nl '\n' << flush
+#define is ==
+#define all(v) (v).begin() , (v).end()
+#define revall(v) (v).rbegin() , (v).rend()
+#define sumof(v) accumulate(all(v), 0LL)
+#define maxof(v) max_element(all(v))
+#define minof(v) min_element(all(v))
+#define rev(v) reverse(all(v))
+#define sz(v)  (long long)(v).size()
+#define lp(i, n) for ( ll i = 0 ; i < (n) ; i++ )
+#define lpd(i, n) for ( ll i = (n)-1 ; i >= 0 ; i-- )
+#define lpc(it, c) for ( auto it = (c).begin(); it != (c).end(); it++)
+#define lpv(val, c) for ( auto val : c )
+#define lpr(i, l, r) for ( ll i = (l) ; i < (r) ; i++ )
+#define lpdr(i, l, r) for ( ll i = (r)-1 ; i >= (l) ; i-- )
+
+#define F first
+#define S second
+#define pb push_back
+#define eb emplace_back
+#define inf INT_MAX
+#define eps 1e-6
+
+#define tc(t) ul testcases ; cin >> testcases ; for(int testcase = 1 ; testcase <= testcases ; testcase++)
+#define prntc cout << "Case #" << testcase << ": ";
+#define prc(c) for (auto it = (c).begin(); it != --((c).end()); it++) cout << *it << " "; cout << *(c.rbegin()) << "\n" << flush;
+#define prv(v) cout << v << "\n" << flush;
+#define invec(vec_name, vec_size) vll vec_name(vec_size); lp(i,vec_size) cin >> vec_name[i];
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define show ; cerr <<
+#define sp ; cerr << "\n";
+#define debug(ds_name) for ( auto val : ds_name ) cerr<<val<<"\n" ; cerr<<"\n";
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__);
+string sep = "-----------------------------\n";
+#else
+#define show ; {}
+#define sp ; {} ;
+#define debug(ds_name) {} ;
+#define pr(...) {} ;
+string sep = "" ;
+#endif
+
+template <class T> ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const pair<S, T>& p) { return os << "(" << p.first << ", " << p.second << ")";}
+template <class S, class T> ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) { cerr << str << " : " << t << "\n"; }
+template <class T, class... S> void dbs(string str, T t, S... s) { int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ", "; dbs(str.substr(idx + 1), s...); }
+
+template <class T> T mx(T t) { return t; } template <class T, class... S> T mx(T t, S... s) { T tt = mx(s...); return (t) > (tt) ? (t) : (tt); }
+template <class T> T mn(T t) { return t; } template <class T, class... S> T mn(T t, S... s) { T tt = mn(s...); return (t) < (tt) ? (t) : (tt); }
+template <class T, class Op> T acc(Op op, T t) { return t; }
+template <class T, class Op, class... S> T acc(Op op, T t, S... s) { return op(t, acc(op, s...)); }
+template <class D1, class D2> auto lambdamax() { return [](D1 a, D2 b){ return (a) > (b) ? (a) : (b) ;} ;}
+template <class D1, class D2> auto lambdamin() { return [](D1 a, D2 b){ return (a) < (b) ? (a) : (b) ;} ;}
+
+ll modExp(ll x,ll y,ll mod = MOD) { x %= mod , y%=(mod-1) ; ll res = 1; while(y){if(y&1) res= (res*x)%mod; y/=2, x=(x*x)%mod; }return res%mod;}
+
+int main()
+{
+    fast
+    tc(t)
+    {
+        int n ; cin >> n ;
+        vec<vi> v(n, vi(n));
+        lp(i,n)
+            lp(j,n) {
+                cin >> v[i][j];
+                v[i][j]--;
+            }
+
+        vec<vi> row(n, vi(n,0));
+        vec<vi> col(n, vi(n,0));
+
+        lp(i,n)
+            lp(j,n){
+                row[v[i][j]][i]++;
+                col[v[i][j]][j]++;
+            }
+
+        vi rowhash(n,0);
+        vi colhash(n,0);
+
+        int trace = 0;
+        lp(i,n)
+            lp(j,n){
+                if(row[i][j] > 1) rowhash[j] = 1;
+                if(col[i][j] > 1) colhash[j] = 1;
+                if(i is j) trace += (v[i][j] + 1);
+            }
+
+        prntc cout << trace << " " << sumof(rowhash) << " " << sumof(colhash) << endl;
+    }
+}
---
+++ Google/CodeJam20/Q-C_ParentingPartneringReturns.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ul ;
+typedef long long ll ;
+typedef string str ;
+typedef pair <int, int >  pii ;
+typedef pair <ll, ll>  pll ;
+typedef vector <int> vi ;
+typedef vector <bool> vb ;
+typedef vector <ll> vll ;
+typedef vector <pii> vpi ;
+typedef vector <pll> vpl ;
+#define umap unordered_map
+
+//#define cerr cout
+#define MOD 1000000007
+#define vec vector
+#define nl '\n' << flush
+#define is ==
+#define all(v) (v).begin() , (v).end()
+#define revall(v) (v).rbegin() , (v).rend()
+#define sumof(v) accumulate(all(v), 0LL)
+#define maxof(v) max_element(all(v))
+#define minof(v) min_element(all(v))
+#define rev(v) reverse(all(v))
+#define sz(v)  (long long)(v).size()
+#define lp(i, n) for ( ll i = 0 ; i < (n) ; i++ )
+#define lpd(i, n) for ( ll i = (n)-1 ; i >= 0 ; i-- )
+#define lpc(it, c) for ( auto it = (c).begin(); it != (c).end(); it++)
+#define lpv(val, c) for ( auto val : c )
+#define lpr(i, l, r) for ( ll i = (l) ; i < (r) ; i++ )
+#define lpdr(i, l, r) for ( ll i = (r)-1 ; i >= (l) ; i-- )
+
+#define F first
+#define S second
+#define pb push_back
+#define eb emplace_back
+#define inf INT_MAX
+#define eps 1e-6
+
+#define tc(t) ul testcases ; cin >> testcases ; for(int testcase = 1 ; testcase <= testcases ; testcase++)
+#define prntc cout << "Case #" << testcase << ": ";
+#define prc(c) for (auto it = (c).begin(); it != --((c).end()); it++) cout << *it << " "; cout << *(c.rbegin()) << "\n" << flush;
+#define prv(v) cout << v << "\n" << flush;
+#define invec(vec_name, vec_size) vll vec_name(vec_size); lp(i,vec_size) cin >> vec_name[i];
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define show ; cerr <<
+#define sp ; cerr << "\n";
+#define debug(ds_name) for ( auto val : ds_name ) cerr<<val<<"\n" ; cerr<<"\n";
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__);
+string sep = "-----------------------------\n";
+#else
+#define show ; {}
+#define sp ; {} ;
+#define debug(ds_name) {} ;
+#define pr(...) {} ;
+string sep = "" ;
+#endif
+
+template <class T> ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const pair<S, T>& p) { return os << "(" << p.first << ", " << p.second << ")";}
+template <class S, class T> ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T> ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) { cerr << str << " : " << t << "\n"; }
+template <class T, class... S> void dbs(string str, T t, S... s) { int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ", "; dbs(str.substr(idx + 1), s...); }
+
+template <class T> T mx(T t) { return t; } template <class T, class... S> T mx(T t, S... s) { T tt = mx(s...); return (t) > (tt) ? (t) : (tt); }
+template <class T> T mn(T t) { return t; } template <class T, class... S> T mn(T t, S... s) { T tt = mn(s...); return (t) < (tt) ? (t) : (tt); }
+template <class T, class Op> T acc(Op op, T t) { return t; }
+template <class T, class Op, class... S> T acc(Op op, T t, S... s) { return op(t, acc(op, s...)); }
+template <class D1, class D2> auto lambdamax() { return [](D1 a, D2 b){ return (a) > (b) ? (a) : (b) ;} ;}
+template <class D1, class D2> auto lambdamin() { return [](D1 a, D2 b){ return (a) < (b) ? (a) : (b) ;} ;}
+
+ll modExp(ll x,ll y,ll mod = MOD) { x %= mod , y%=(mod-1) ; ll res = 1; while(y){if(y&1) res= (res*x)%mod; y/=2, x=(x*x)%mod; }return res%mod;}
+
+int main()
+{
+    fast
+    tc(t)
+    {
+        ll n ; cin >> n ;
+
+        vec<pair<pii,int>> v(n);
+        lp(i,n) {
+            int s, e; cin >> s >> e;
+            v.pb({{s, +1}, i});
+            v.pb({{e, -1}, i});
+        }
+        sort(all(v));
+
+        int count = 0;
+        int lastend, laststart = 1;
+        char ans[] = {'C', 'J'};
+        string schedule(n, 0);
+        lp(i, sz(v))
+        {
+            count += v[i].F.S;
+            if (count is 3) {
+                break;
+            }
+
+            if (v[i].F.S is -1) {
+                lastend = schedule[v[i].S];
+                if (lastend is laststart) laststart = !laststart;
+            }
+            else if (v[i].F.S is 1) {
+                schedule[v[i].S] = !laststart;
+                laststart = !laststart;
+            }
+        }
+
+        if (count is 3){
+            schedule = "IMPOSSIBLE";
+        } else {
+            transform(all(schedule), schedule.begin(), [ans](int x){return ans[x];} );
+        }
+
+        prntc cout << schedule << endl;
+    }
+}
